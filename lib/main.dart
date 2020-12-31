2db7d0be7639f8d76cabcf77306d4a32c8b14a2d
Lucas William <lucaswilliameufrasio@gmail.com>
2020-12-31 02:38:07 -0300

    feat: get authorization code from gitlab

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
+import 'dart:convert';
+
+import 'package:crypto/crypto.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:oauth2_client/authorization_response.dart';
 import 'package:oauth2_client/oauth2_client.dart';
-import 'package:flutter_config/flutter_config.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:uuid/uuid.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized(); // Required by FlutterConfig
-  await FlutterConfig.loadEnvVariables();
+  await DotEnv().load('.env');
 
   runApp(MyApp());
 }
@@ -16,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Github OAuth2 Flutter',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -36,32 +39,35 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String clientId = FlutterConfig.get('GITHUB_CLIENT_ID');
-  // NEVER STORE THE CLIENT SECRET ON THE APP, 
+  String githubClientId = DotEnv().env['GITHUB_CLIENT_ID'];
+  // NEVER STORE THE CLIENT SECRET ON THE APP,
   // move the responsibility to the backend get you the access token
-  String clientSecret = FlutterConfig.get('GITHUB_CLIENT_SECRET');
+  String githubClientSecret = DotEnv().env['GITHUB_CLIENT_SECRET'];
+  String gitlabClientId = DotEnv().env['GITLAB_CLIENT_ID'];
+  String gitlabClientSecret = DotEnv().env['GITLAB_CLIENT_SECRET'];
+  String codeVerifier = Uuid().v4();
 
   Future<void> authenticate() async {
     try {
-      OAuth2Client aclient = OAuth2Client(
+      OAuth2Client client = OAuth2Client(
         authorizeUrl: 'https://github.com/login/oauth/authorize',
         tokenUrl: 'https://github.com/login/oauth/access_token',
         redirectUri: 'dev.lucaswilliameufrasio.flutteroauth://oauth2redirect',
         customUriScheme: 'dev.lucaswilliameufrasio.flutteroauth',
       );
-      aclient.accessTokenRequestHeaders = {'Accept': 'application/json'};
+      client.accessTokenRequestHeaders = {'Accept': 'application/json'};
 
-      await aclient.getTokenWithAuthCodeFlow(
-        clientId: clientId,
+      await client.getTokenWithAuthCodeFlow(
+        clientId: githubClientId,
         scopes: ['identity'],
-        afterAuthorizationCodeCb: getAccessToken,
+        afterAuthorizationCodeCb: getGithubAccessToken,
       );
     } catch (error) {
       print("Error: $error");
     }
   }
 
-  Future<void> getAccessToken(
+  Future<void> getGithubAccessToken(
       AuthorizationResponse authorizationResponse) async {
     print(authorizationResponse.code);
 
@@ -69,12 +75,64 @@ class _MyHomePageState extends State<MyHomePage> {
     // send the code (authorizationResponse.code) to the backend
     var params = {
       "code": authorizationResponse.code,
-      "client_id": clientId,
-      "client_secret": clientSecret
+      "client_id": githubClientId,
+      "client_secret": githubClientSecret
     };
     var uri = Uri.https('github.com', 'login/oauth/access_token', params);
     var accessToken = await http.post(uri);
     print(accessToken.body);
+  }
+
+  Future<void> authenticateToGitlab() async {
+    var codeVerifierUtf8Encoded = utf8.encode(codeVerifier);
+    var codeVerifierDigest = sha256.convert(codeVerifierUtf8Encoded);
+    var codeChallenge =
+        Uri.encodeComponent(base64Encode(codeVerifierDigest.bytes));
+    try {
+      OAuth2Client client = OAuth2Client(
+        authorizeUrl: 'https://gitlab.com/oauth/authorize',
+        tokenUrl: 'https://gitlab.com/oauth/token',
+        redirectUri: 'dev.lucaswilliameufrasio.flutteroauth://oauth2redirect',
+        customUriScheme: 'dev.lucaswilliameufrasio.flutteroauth',
+      );
+      client.accessTokenRequestHeaders = {'Accept': 'application/json'};
+      print(client.redirectUri);
+
+      await client.getTokenWithAuthCodeFlow(
+        clientId: gitlabClientId,
+        authCodeParams: {
+          "code_challenge": codeChallenge,
+          "code_challenge_method": "S256",
+        },
+        accessTokenParams: {
+          "code_verifier": codeVerifier,
+        },
+        scopes: ['read_user'],
+        afterAuthorizationCodeCb: getGitlabAccessToken,
+      );
+
+    } catch (error) {
+      print("Error: $error");
+    }
+  }
+
+  Future<void> getGitlabAccessToken(
+      AuthorizationResponse authorizationResponse) async {
+    print(authorizationResponse.code);
+
+    // This need to be responsibility of backend
+    // send the code (authorizationResponse.code) to the backend
+    // var params = {
+    //   "client_id": gitlabClientId,
+    //   "client_secret": gitlabClientSecret,
+    //   "code": authorizationResponse.code,
+    //   "grant_type": "authorization_code",
+    //   "redirect_uri": "dev.lucaswilliameufrasio.flutteroauth://oauth2redirect",
+    //   "code_verifier": codeVerifier,
+    // };
+    // var uri = Uri.https('gitlab.com', '/oauth/token', params);
+    // var accessToken = await http.post(uri);
+    // print(accessToken.body);
   }
 
   @override
@@ -108,7 +166,26 @@ class _MyHomePageState extends State<MyHomePage> {
                     const Text('Authenticate', style: TextStyle(fontSize: 20)),
               ),
               onPressed: authenticate,
-            )
+            ),
+            RaisedButton(
+              padding: const EdgeInsets.all(0.0),
+              textColor: Colors.white,
+              child: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: <Color>[
+                      Color(0xFF0D47A1),
+                      Color(0xFF1976D2),
+                      Color(0xFF42A5F5),
+                    ],
+                  ),
+                ),
+                padding: const EdgeInsets.all(10.0),
+                child: const Text('Authenticate to Gitlab',
+                    style: TextStyle(fontSize: 20)),
+              ),
+              onPressed: authenticateToGitlab,
+            ),
           ],
         ),
       ),
